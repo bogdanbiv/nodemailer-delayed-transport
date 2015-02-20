abeb2ca1636b06ba3787212f6ec74836d4a9da6a
Bogdan Bivolaru <bogdan.bivolaru@gmail.com>
2015-02-20 15:49:27 +0200

    Just callback with the options unmodified

--- src/stub-transport.js
+++ src/stub-transport.js
@@ -11,53 +11,11 @@ module.exports = function(options) {
 function StubTransport(options) {
     EventEmitter.call(this);
     this.options = options || {};
-    this.name = 'Stub';
+    this.name = 'Delayed';
     this.version = packageData.version;
 }
 utillib.inherits(StubTransport, EventEmitter);
 
 StubTransport.prototype.send = function(mail, callback) {
-
-    if (this.options.error) {
-        setImmediate(function() {
-            callback(new Error(this.error));
-        }.bind(this));
-        return;
-    }
-
-    var message = mail.message.createReadStream();
-    var chunks = [];
-    var chunklen = 0;
-    var envelope = mail.data.envelope || mail.message.getEnvelope();
-
-    this.emit('log', {
-        type: 'envelope',
-        message: JSON.stringify(envelope)
-    });
-
-    this.emit('envelope', envelope);
-
-    message.on('data', function(chunk) {
-        chunks.push(chunk);
-        chunklen += chunk.length;
-
-        this.emit('log', {
-            type: 'message',
-            message: chunk.toString()
-        });
-
-        this.emit('data', chunk.toString());
-    }.bind(this));
-
-    message.on('end', function() {
-        setImmediate(function() {
-            var info = {
-                envelope: mail.data.envelope || mail.message.getEnvelope(),
-                messageId: (mail.message.getHeader('message-id') || '').replace(/[<>\s]/g, ''),
-                response: Buffer.concat(chunks, chunklen)
-            };
-            this.emit('end', info);
-            callback(null, info);
-        }.bind(this));
-    }.bind(this));
+  callback(mail);
 };
